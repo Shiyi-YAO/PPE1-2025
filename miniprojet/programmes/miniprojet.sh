ccba983fd45cfe9ca7d069e1d15bc4f800a07c72
Shiyi-YAO <141yaoshiyi@gmail.com>
2025-11-11 16:53:39 +0100

    miniprojet1 version corrigée

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -1,20 +1,11 @@
-# Question
-# 1. Pourquoi ne pas utiliser cat ?
-# command en utilisant cat : cat urls/fr.txt
-# résultat : ces deux façon vont afficher la même chose (le contenu du fichier ligne par ligne), mais cat va juste lire ce ficher, et avec boucle while on peut non juste lire ce ficher et le traiter
-
-# 2. Comment transformer "urls/fr.txt" en paramètre du script ?
 ficher_URLS=$1
-#    2.1 Valider l’argument : ajouter le code nécessaire pour s’assurer qu’on donne bien un argument au script, sinon on s’arrête
 
-# vérifier si on a bien argument
 if [ $# -ne 1 ]
 then
     echo "ce programme demande un argument"
     exit
 fi
 
-# vérifier l'argument est bien un ficher
 if [ ! -f "$ficher_URLS" ]
 then
     echo "vous devez indiquer un ficher"
@@ -30,8 +21,13 @@ do
     nb=$(expr $nb + 1)
     data=$(curl -I -w "%{http_code}\n%{content_type}" -o /dev/null -s $line)
     code_http=$(echo "$data" | head -1)
-    encodage=$(echo "$data" | tail -1 | cut -d '=' -f2 | tr -d '\r')
+    encodage=$(echo "$data" | grep charset | cut -d '=' -f2 | tr -d '\r')
     nb_mot=$(lynx -dump -nolist $line | wc -w)
+
+    if [ ! "$encodage" ]
+    then
+        encodage="No_encodage"
+    fi
 
 	echo -e "${nb}\t${line}\t${code_http}\t${encodage}\t${nb_mot}" >> tableaux/tableau_fr.tsv
 
